fcc4baa3dc6c0372a4c54a063433cec41a6dca91
Axel Pinçon <axelpincon@MacBook-Pro-de-Axel.local>
2023-03-09 16:39:08 +0100

    modif test q1

--- tests/test_s1q1_graph_loading.py
+++ tests/test_s1q1_graph_loading.py
@@ -15,12 +15,6 @@ class Test_GraphLoading(unittest.TestCase):
         g = graph_from_file("input/network.01.in")
         self.assertEqual(g.nb_nodes, 7)
         self.assertEqual(g.nb_edges, 5)
-    
-    def test_network4(self):
-        g = graph_from_file("input/network.04.in")
-        self.assertEqual(g.nb_nodes, 10)
-        self.assertEqual(g.nb_edges, 4)
-        self.assertEqual(g.graph[1][0][2], 6)
 
 if __name__ == '__main__':
     unittest.main()
